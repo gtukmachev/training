4084acb06e392581b617a0fac0eb09f512867961
Grigory <grigory@clearscale.net>
2021-08-14 16:59:46 +0700

    brackets

---
+++ src/main/kotlin/tga/training/brackets/brackets.kt
@@ -0,0 +1,44 @@
+package tga.training.brackets
+
+fun main(args : Array<String>) {
+
+    test("([{}])", true)
+    test("([{])", false)
+
+}
+
+fun test(input: String, expectedResult: Boolean) {
+    val task = "$input -> $expectedResult"
+    val actualResult = isBalanced(input)
+
+    if (actualResult == expectedResult) {
+        println("\u2713 : $task"); return
+    }
+
+    println("\u2717 : $task   | actual result: $actualResult")
+
+}
+
+fun isBalanced(str: String): Boolean {
+    val stack = Array<Char>(str.length){' '}
+    var i = 0
+
+    str.forEach{ ch ->
+        when (ch) {
+            '(', '[', '{' -> stack[i++] = ch
+            ')', ']', '}' -> {
+                if (i == 0) return false
+                val machSym = when (stack[--i]) {
+                    '(' -> ')'
+                    '[' -> ']'
+                    '{' -> '}'
+                    else -> '_'
+                }
+                if (ch != machSym) return false
+            }
+            else -> return false
+        }
+    }
+
+    return i == 0
+}
