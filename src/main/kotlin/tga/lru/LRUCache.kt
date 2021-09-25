e939a60df66c550660f579b15a7b35dec020d27f
Grigory <grigory@clearscale.net>
2021-09-25 17:19:00 +0700

    lru

--- src/main/kotlin/tga/lru/LRUCache.kt
+++ src/main/kotlin/tga/lru/LRUCache.kt
@@ -2,44 +2,73 @@ package tga.lru
 
 class LRUCache(val capacity: Int) {
 
-    private class Node(var key: Int = -1, var value: Int = -1, var next: Node? = null){
+    private class Node(var key: Int = -1, var value: Int = -1, var next: Node? = null, var prev: Node? = null){
         override fun toString(): String {
-            return "[$value -> ${next?.value ?: "." }]"
+            return "[${prev?.value ?: "." } < $value > ${next?.value ?: "." }]"
         }
     }
 
+    var size: Int = 0
+        private set
 
-    private var head = Node()
+    private var head = Node()                                    // the `head` and `last` are barriers:
+    private var last = Node(prev = head).also { head.next = it } // `head` <-> 1 <-> 2 <-> ... <-> `last`
+
     private val index = HashMap<Int, Node>() // key -> Node(value, ...)
 
     operator fun set(key: Int, value: Int) = put(key, value)
 
     operator fun get(key: Int): Int {
-        val parentNode = index[key] ?: return -1
+        val keyNode = index[key] ?: return -1
 
-        val keyNode = parentNode.next!!
+        val prevNode = keyNode.prev!!
+        val nextNode = keyNode.next!!
 
-        parentNode.next = keyNode.next
+        // move the current element to top of the list
+
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
+
         keyNode.next = head.next
+        keyNode.prev = head
+
+        head.next!!.prev = keyNode
         head.next = keyNode
 
-        index[key] = head
-        if (keyNode.next != null) index[keyNode.next!!.key] = keyNode
+        index[key] = keyNode
 
         return keyNode.value
     }
 
 
     fun put(key: Int, value: Int) {
+        val existsValue = get(key)
+        if (existsValue != -1) return
+
         val keyNode = head
         keyNode.key = key
         keyNode.value = value
 
         head = Node(next = keyNode)
-        index[key] = head
+        keyNode.prev = head
+
+        index[key] = keyNode
+
+        size++
+
+        if (size > capacity) {
+
+            val beforeLast = last.prev!!
+            index.remove(beforeLast.key)
+
+            val beforeBeforeLast = last.prev!!.prev!!
+            beforeBeforeLast.next = last
+            last.prev = beforeBeforeLast
+
+            size--
+        }
+        return
     }
-
-
 
 }
 
--- src/main/kotlin/tga/lru/LRUCacheTests.kt
+++ src/main/kotlin/tga/lru/LRUCacheTests.kt
@@ -6,6 +6,31 @@ import org.junit.Test
 class LRUCacheTests {
 
 
+    @Test
+    fun test0() {
+        val cache = LRUCache(0)
+
+        cache[1] = 1
+        assertThat( cache[1] ).isEqualTo(-1)
+
+        cache[2] = 2
+        assertThat( cache[2] ).isEqualTo(-1)
+    }
+
+    @Test
+    fun test1() {
+        val cache = LRUCache(1)
+
+        cache[1] = 1
+        assertThat( cache[1] ).isEqualTo(1)
+        assertThat( cache[1] ).isEqualTo(1)
+
+        cache[2] = 2
+        assertThat( cache[1] ).isEqualTo(-1)
+        assertThat( cache[2] ).isEqualTo(2)
+        assertThat( cache[2] ).isEqualTo(2)
+        assertThat( cache[1] ).isEqualTo(-1)
+    }
 
     @Test
     fun test2() {
